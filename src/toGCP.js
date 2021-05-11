b6199c72d68a339d44857bbab0a329ca1016227e
Brian <brianflaherty64@gmail.com>
2021-05-11 00:26:54 -0400

    feat: extended and debugged GCP operator API

--- src/toGCP.js
+++ src/toGCP.js
@@ -3,14 +3,27 @@ import {map} from 'rxjs/operators';
 import speech from '@google-cloud/speech';
 import {rxToStream} from 'rxjs-stream';
 
-const defaultConfig = {
-  encoding: 'LINEAR16',
-  sampleRateHertz: 16000,
-  languageCode: 'en-US',
-  enableWordConfidence: true,
-  enableWordTimeOffsets: true,
-  enableSpeakerDiarization: true,
-  enableAutomaticPunctuation: true,
+// const defaultConfig = {
+  // encoding: 'LINEAR16',
+  // sampleRateHertz: 16000,
+  // languageCode: 'en-US',
+  // enableWordConfidence: true,
+  // enableWordTimeOffsets: true,
+  // enableSpeakerDiarization: true,
+  // enableAutomaticPunctuation: true,
+// };
+
+const parseEncoding = encoding => {
+  switch (encoding) {
+    case 'audio/l16':
+      return 'LINEAR16';
+    case 'audio/flac':
+      return 'FLAC';
+    case 'audio/mpeg3':
+      return 'MP3';
+    default:
+      return 'LINEAR16';
+  }
 };
 
 const createSpeechClient = (_speech = speech) => new _speech.SpeechClient();
@@ -19,18 +32,45 @@ const createSpeechClient = (_speech = speech) => new _speech.SpeechClient();
 // fileChunk$ must be a file format compatible with GCP's speech-to-text API,
 // like LINEAR 16 (16-bit PCM encoded audio data at a sample rate of 16000)
 const toGCP = ({
-  config = defaultConfig,
-  interimResults = false,
+  // config = defaultConfig,
+  contentType = 'audio/l16',
+  sampleRate = 16000,
+  channels = null,
+  languageCode = 'en-US',
+  maxSpeakerCount = 5,
+  useInterimResults = false,
+  useSpeakerLabels = true,
+  usePunctuation = true,
+  useWordConfidence = true,
+  useTimeOffsets = true,
+  singleUtterance = false,
   _createSpeechClient = createSpeechClient,
   _rxToStream = rxToStream
 } = {}) => fileChunk$ => {
   const stt$ = new Observable(obs => {
     const client = _createSpeechClient();
     const base64Chunk$ = fileChunk$.pipe(
-      map(chunk => chunk.toString('base64'))
+      map(chunk => chunk.toString('base64')),
     );
     const readStream = _rxToStream(base64Chunk$);
-    const sttPipeline = client.streamingRecognize({config, interimResults});
+    // https://googleapis.dev/nodejs/speech/latest/google.cloud.speech.v1p1beta1.IStreamingRecognitionConfig.html
+    const sttPipeline = client.streamingRecognize({
+      singleUtterance,
+      config: {
+        languageCode,
+        maxSpeakerCount,
+        encoding: parseEncoding(contentType),
+        sampleRateHertz: sampleRate,
+        audioChannelCount: channels,
+        enableSpeakerDiarization: useSpeakerLabels,
+        enableAutomaticPunctuation: usePunctuation,
+        enableWordConfidence: useWordConfidence,
+        enableWordTimeOffsets: useTimeOffsets,
+        // model
+        // useEnhanced
+      },
+      interimResults: useInterimResults,
+    });
     readStream.pipe(sttPipeline)
       .on('error', err => obs.error(err))
       .on('data', data => obs.next(data))
@@ -39,4 +79,7 @@ const toGCP = ({
   return stt$;
 };
 
+export const testExports = {
+  parseEncoding
+};
 export default toGCP;
--- src/toGCP.test.js
+++ src/toGCP.test.js
@@ -2,7 +2,8 @@ import {expect} from 'chai';
 import sinon from 'sinon';
 import {marbles} from 'rxjs-marbles/mocha';
 
-import toGCP from './toGCP';
+import toGCP, {testExports} from './toGCP';
+const {parseEncoding} = testExports;
 
 // Not much point in writing tests for this, since they'd basically just be
 // integration tests
@@ -10,4 +11,9 @@ describe('toGCP', () => {
   it('should export a function', marbles(m => {
     expect(toGCP).to.be.a('function');
   }));
+
+  it('should parseEncoding properly', () => {
+    const codec = parseEncoding('audio/l16');
+    expect(codec).to.deep.equal('LINEAR16');
+  });
 });
