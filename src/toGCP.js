67d27a3ab86d1a2b04451ab37e1b60e0cac2a88b
Brian <brianflaherty64@gmail.com>
2021-05-11 20:21:38 -0400

    feat: added support for word-level confidence and beta features

--- src/toGCP.js
+++ src/toGCP.js
@@ -1,6 +1,6 @@
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
-import speech from '@google-cloud/speech';
+import speech, {v1p1beta1} from '@google-cloud/speech';
 import {rxToStream} from 'rxjs-stream';
 
 // const defaultConfig = {
@@ -27,6 +27,7 @@ const parseEncoding = encoding => {
 };
 
 const createSpeechClient = (_speech = speech) => new _speech.SpeechClient();
+const createBetaClient = (_speech = v1p1beta1) => new _speech.SpeechClient();
 
 // See https://cloud.google.com/speech-to-text/docs/streaming-recognize
 // fileChunk$ must be a file format compatible with GCP's speech-to-text API,
@@ -43,12 +44,14 @@ const toGCP = ({
   usePunctuation = true,
   useWordConfidence = true,
   useTimeOffsets = true,
+  useBetaModel = true,
   singleUtterance = false,
   _createSpeechClient = createSpeechClient,
+  _createBetaClient = createBetaClient,
   _rxToStream = rxToStream
 } = {}) => fileChunk$ => {
   const stt$ = new Observable(obs => {
-    const client = _createSpeechClient();
+    const client = useBetaModel ? _createBetaClient() : _createSpeechClient();
     const base64Chunk$ = fileChunk$.pipe(
       map(chunk => chunk.toString('base64')),
     );
