e38fdb832155dfec841f5b34c0bdcbc8daa2b3d8
Brian <brianflaherty64@gmail.com>
2021-11-02 12:44:15 -0400

    tests: added realistic audio test to timing operator

--- src/internals/trackAudioTimeConsumed.test.js
+++ src/internals/trackAudioTimeConsumed.test.js
@@ -1,8 +1,17 @@
+import path from 'path';
 import {expect} from 'chai';
-// import sinon from 'sinon';
+import sinon from 'sinon';
 import {marbles} from 'rxjs-marbles/mocha';
+import {map} from 'rxjs/operators'
+
+import {fromFile} from '@bottlenose/rxfs';
 
 import trackAudioTimeConsumed from './trackAudioTimeConsumed';
+
+const SAMPLE_FILE_PATH = path.resolve(
+  __dirname,
+  '../../scripts/sample-audio.linear16'
+);
 
 describe('trackAudioTimeConsumed', () => {
   it('should export a function', () => {
@@ -21,4 +30,17 @@ describe('trackAudioTimeConsumed', () => {
     const result$ = source$.pipe(trackAudioTimeConsumed());
     m.expect(result$).toBeObservable(expected$);
   }));
+
+  it('should correctly time the real audio sample file', done => {
+    const onData = sinon.spy();
+    const source$ = fromFile({filePath: SAMPLE_FILE_PATH});
+    const out$ = source$.pipe(
+      trackAudioTimeConsumed()
+    );
+    const actualAudioTime = 98.97800000000008;
+    out$.subscribe(onData, console.trace, () => {
+      expect(onData.getCall(onData.callCount - 1).args[0]).to.equal(actualAudioTime);
+      done();
+    });
+  }).timeout(3000);
 });
